1024c1c65ed48fde86f237fdaa658d2145a1b522
nech <sowul789@gmail.com>
2024-01-02 18:48:58 +0300

    removed the artificial delay

--- src/pages/tasks/index.ts
+++ src/pages/tasks/index.ts
@@ -1,8 +1,3 @@
 import { lazy } from "react";
 
-export const TasksAsync = lazy(
-  () =>
-    new Promise((resolve) => {
-      setTimeout(() => resolve(import("./ui/Tasks")), 1000);
-    })
-);
+export const TasksAsync = lazy(() => import("./ui/Tasks"));
